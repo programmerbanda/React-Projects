bfe2fe72c388ac3a95dcc86086a2a29ff8e6ad1d
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-08-25 18:08:48 +0530

    update

--- Fetch-API/src/App.jsx
+++ Fetch-API/src/App.jsx
@@ -1,16 +1,36 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { getPosts } from "./api";
+import { getPosts, getRandomUser } from "./api";
+import PostCard from "./components/PostCard";
+import UserCard from "./components/UserCard";
 
 function App() {
   const [data, setData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     getPosts().then((post) => setData(post));
   }, []);
 
-  return( <div>{data ? data.map(e => <li>{e.title}</li>) : <p>No Available Data</p>}</div>);
+  useEffect(() => {
+    getRandomUser().then((user) => setUserData(user.results[0]));
+  }, []);
+
+  const refresh = () => {
+    getRandomUser().then((user) => setUserData(user.results[0]));
+  }
+
+  return (
+    <div>
+      {userData && <UserCard data={userData} />}
+      <button onClick={refresh}>Refresh Here</button>
+      {data ? (
+        data.map((e) => <PostCard title={e.title} body={e.body} />)
+      ) : (
+        <p>No Available Data</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ Fetch-API/src/components/PostCard.jsx
@@ -0,0 +1,10 @@
+const PostCard = (props) => {
+    return (
+        <div className="post-card">
+            <h3>{props.title}</h3>
+            <p>{props.body}</p>
+        </div>
+    )
+}
+
+export default PostCard;
