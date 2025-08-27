6fb0ec37cf37cdb16f591a67da4e9faeadea2ead
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-08-27 18:59:28 +0530

    learning react rounter

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import HomePage from './App'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
